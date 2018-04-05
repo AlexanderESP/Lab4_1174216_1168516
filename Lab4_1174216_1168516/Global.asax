d1cb2738a1633b8d86817737606953335ff51cca
Alexander Sánchez <36433369+AlexanderESP@users.noreply.github.com>
2018-04-05 07:46:46 -0600

    Add project files.

---
+++ Lab4_1174216_1168516/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab4_1174216_1168516.WebApiApplication" Language="C#" %>
---
+++ Lab4_1174216_1168516/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Lab4_1174216_1168516
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
